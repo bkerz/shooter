b3b39b211fd08423094786242820e82b9e651e14
Beicker Zambrano <beicker998@gmail.com>
2024-04-06 19:38:05 -0500

    feat: show doors following minimap scheme
    Not only show doors but also connect them to a signal to allow switching
    to a new room

--- level.gd
+++ level.gd
@@ -2,11 +2,54 @@ extends Node2D
 
 signal exit_level
 
+var doors: Dictionary = {
+		"right": false,
+		"left": false,
+		"top": false,
+		"bottom": false,
+	}
+
 func _ready():
+	var door_right: Area2D = get_node("DoorRight")
+	door_right.visible = false
+
+	var door_top: Area2D = get_node("DoorTop")
+	door_top.visible = false
+
+	var door_left: Area2D = get_node("DoorLeft")
+	door_left.visible = false
+
+	var door_bottom: Area2D = get_node("DoorBottom")
+	door_bottom.visible = false
+
+	if doors["right"]:
+		door_right.visible = true
+		door_right.leave.connect(_on_right_door_leave)
+	if doors["top"]:
+		door_top.visible = true
+		door_top.leave.connect(_on_top_door_leave)
+	if doors["left"]:
+		door_left.visible = true
+		door_left.leave.connect(_on_left_door_leave)
+	if doors["bottom"]:
+		door_bottom.visible = true
+		door_bottom.leave.connect(_on_bottom_door_leave)
 	pass
 
-
-func _on_door_leave():
-	exit_level.emit()
+func _on_top_door_leave():
+	exit_level.emit(Vector2.UP)
 	pass # Replace with function body.
+
+func _on_right_door_leave():
+	exit_level.emit(Vector2.RIGHT)
+	pass # Replace with function body.
+
+func _on_left_door_leave():
+	exit_level.emit(Vector2.LEFT)
+	pass # Replace with function body.
+
+func _on_bottom_door_leave():
+	exit_level.emit(Vector2.DOWN)
+	pass # Replace with function body.
+
 
